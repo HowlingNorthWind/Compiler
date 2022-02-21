43404802d021660cfd51666525d917bf37e02f4f
JingzeLiu99 <178373892@qq.com>
2022-02-21 11:16:07 +0800

    design ast

---
+++ src/ast.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <memory>
+#include <string>
+// 所有 AST 的基类
+class BaseAST {
+ public:
+  virtual ~BaseAST() = default;
+};
+
+// CompUnit 是 BaseAST
+class CompUnitAST : public BaseAST {
+ public:
+  // 用智能指针管理对象
+  std::unique_ptr<BaseAST> func_def;
+};
+
+// FuncDef 也是 BaseAST
+class FuncDefAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> func_type;
+  std::string ident;
+  std::unique_ptr<BaseAST> block;
+};
+
+// FuncType 也是 BaseAST
+class FuncTypeAST : public BaseAST {
+ public:
+  
+};
+
+// Block 也是 BaseAST
+class BlockAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> stmt;
+};
+
+// Stmt 也是 BaseAST
+class StmtAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> number;
+};
+
+// Number 也是 BaseAST
+class NumberAST : public BaseAST {
+ public:
+   std::string int_const;
+};
